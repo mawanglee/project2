7fc82c4005fff541175a3e905c365868417052f6
이 민엽 <mylee@soosungeng.com>
2021-09-14 11:01:01 +0900

    init

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+var element = React.createElement('h1', {className : 'greeting'}, 'Hello, World!');
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  element,  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
